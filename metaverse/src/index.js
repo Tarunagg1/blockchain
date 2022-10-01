6caa87c3638f0cd8e468fcaa7176e42cd4a16f08
tarun <arunaggarwal096@gmail.com>
2022-10-01 12:19:06 +0530

    metaverse application

--- metaverse/src/index.js
+++ metaverse/src/index.js
@@ -1,7 +1,6 @@
-// import Movements from "./movement.js";
-
-// import polygon from "./Web3.js";
-// import abi from "./abi/abi.json" assert { type: "json" };
+import Movements from "./movement.js";
+import polygon from "./Web3.js";
+import abi from "./abi/abi.json" assert { type: "json" };
 
 const scene = new THREE.Scene();
 //Contract Address = 0x9647D18bFd52F007F382804910A887c580E4f083
@@ -37,11 +36,11 @@ const cylinder = new THREE.Mesh(geometry_cyl, material_cyl);
 scene.add(cylinder);
 cylinder.position.set(20, 5, 0);
 
-const geometry_cone = new THREE.ConeGeometry(5, 20, 32);
-const material_cone = new THREE.MeshPhongMaterial({ color: 0x1be3ef });
-const cone = new THREE.Mesh(geometry_cone, material_cone);
-scene.add(cone);
-cone.position.set(-10, 5, 0);
+// const geometry_cone = new THREE.ConeGeometry(5, 20, 32);
+// const material_cone = new THREE.MeshPhongMaterial({ color: 0x1be3ef });
+// const cone = new THREE.Mesh(geometry_cone, material_cone);
+// scene.add(cone);
+// cone.position.set(-10, 5, 0);
 
 camera.position.z = 5;
 camera.position.set(10, 5, 40);
@@ -52,28 +51,28 @@ function animate() {
   cube.rotation.z += 0.05;
   cylinder.rotation.x += 0.1;
   cylinder.rotation.y += 0.1;
-  cone.rotation.x += 0.1;
-  cone.rotation.y += 0.1;
+  // cone.rotation.x += 0.1;
+  // cone.rotation.y += 0.1;
   requestAnimationFrame(animate);
 
-//   if (Movements.isPressed(37)) {
-//     //left
-//     camera.position.x -= 0.5;
-//   }
-//   if (Movements.isPressed(38)) {
-//     //up
-//     camera.position.x += 0.5;
-//     camera.position.y += 0.5;
-//   }
-//   if (Movements.isPressed(39)) {
-//     //right
-//     camera.position.x += 0.5;
-//   }
-//   if (Movements.isPressed(40)) {
-//     //down
-//     camera.position.x -= 0.5;
-//     camera.position.y -= 0.5;
-//   }
+  if (Movements.isPressed(37)) {
+    //left
+    camera.position.x -= 0.5;
+  }
+  if (Movements.isPressed(38)) {
+    //up
+    camera.position.x += 0.5;
+    camera.position.y += 0.5;
+  }
+  if (Movements.isPressed(39)) {
+    //right
+    camera.position.x += 0.5;
+  }
+  if (Movements.isPressed(40)) {
+    //down
+    camera.position.x -= 0.5;
+    camera.position.y -= 0.5;
+  }
 
   camera.lookAt(area.position);
   renderer.render(scene, camera);
@@ -81,19 +80,50 @@ function animate() {
 animate();
 renderer.render(scene, camera);
 
-// const button = document.querySelector("#mint");
-// button.addEventListener("click", mintNFT);
+const button = document.querySelector("#mint");
+button.addEventListener("click", mintNFT);
 
+async function mintNFT() {
+  let nft_name = document.querySelector("#nft_name").value;
+  let nft_width = document.querySelector("#nft_width").value;
+  let nft_height = document.querySelector("#nft_height").value;
+  let nft_depth = document.querySelector("#nft_depth").value;
+  let nft_x = document.querySelector("#nft_x").value;
+  let nft_y = document.querySelector("#nft_y").value;
+  let nft_z = document.querySelector("#nft_z").value;
 
-// polygon.then((result) => {
-//   result.nft.forEach((object, index) => {
-//     if (index <= result.supply) {
-//       const geometry_cube = new THREE.BoxGeometry(object.w, object.h, object.d);
-//       const material_cube = new THREE.MeshPhongMaterial({ color: 0x1be3ef });
-//       const nft = new THREE.Mesh(geometry_cube, material_cube);
+  if (typeof window.ethereum == "undefined") {
+    rej("You should install Metamask");
+  }
 
-//       nft.position.set(object.x, object.y, object.z);
-//       scene.add(nft);
-//     }
-//   });
-// });
+  let web3 = new Web3(window.ethereum);
+  let contract = new web3.eth.Contract(
+    abi,
+    "0x82573B0718D4861eBdF99cA32C2cc166F27934f1"
+  );
+
+  web3.eth.requestAccounts().then((accounts) => {
+    contract.methods
+      .mint(nft_name, nft_width, nft_height, nft_depth, nft_x, nft_y, nft_z)
+      .send({
+        from: accounts[0],
+        value: "10",
+      })
+      .then((data) => {
+        console.log("NFT is minted");
+      });
+  });
+}
+
+polygon.then((result) => {
+  result.nft.forEach((object, index) => {
+    if (index <= result.supply) {
+      const geometry_cube = new THREE.BoxGeometry(object.w, object.h, object.d);
+      const material_cube = new THREE.MeshPhongMaterial({ color: 0x1be3ef });
+      const nft = new THREE.Mesh(geometry_cube, material_cube);
+
+      nft.position.set(object.x, object.y, object.z);
+      scene.add(nft);
+    }
+  });
+});
